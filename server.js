bb04707e97e52d6ff327fea9e7f2afed6839b5f5
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-06-07 00:48:15 -0700

    basic structure set up

---
+++ server.js
@@ -0,0 +1,13 @@
+var path = require('path')
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+var listener = app.listen(process.env.PORT || 8000, function() {
+    console.log('Test server running on ' + listener.address().port);
+});
