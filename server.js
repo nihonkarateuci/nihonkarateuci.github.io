cf5bb93ec652e003b41e336e113d3f5a87d76f78
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-06-08 15:12:32 -0700

    Footer basics

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/images')
+
 
 var listener = app.listen(process.env.PORT || 8000, function() {
     console.log('Test server running on ' + listener.address().port);
