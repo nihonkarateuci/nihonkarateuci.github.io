55e75d562339851d400056ba0bdda97b26498293
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-06-19 22:24:48 -0700

    Added some mobile support for the home page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-console.log("index.js has been loaded")
+console.log("index.js has been loaded");
+
+function openMenu() {
+    document.getElementById('menu').style.height = '100%';
+}
+
+function closeMenu() {
+    document.getElementById('menu').style.height = '0%';
+}
