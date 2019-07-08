42e7d2f1fb40ff8635346c84f639fd823bcd463a
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-07-07 20:16:18 -0700

    Mobile improvements

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
-function openMenu() {
-    document.getElementById('menu').style.height = '100%';
-}
-
-function closeMenu() {
-    document.getElementById('menu').style.height = '0%';
+function toggleMenu() {
+    let menu = document.getElementById("menu");
+    let button = document.getElementById("toggle-button");
+    if (button.innerHTML == "\u2630") {
+        button.innerHTML = "&times;";
+        button.style.top = "-0.35rem";
+        menu.style.height = "100%";
+    } else {
+        button.innerHTML = "&#9776;";
+        button.style.top = "0";
+        menu.style.height = "0%";
+    }
 }
