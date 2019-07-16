fda974e3661448ce10fa2fb94fa483bf94c39066
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-07-15 23:35:27 -0700

    First iteration of circle menu on index.html

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,17 @@ function toggleMenu() {
         menu.style.height = "0%";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const items = document.querySelectorAll('.circle a');
+
+    for(let i = 0, l = items.length; i < l; i++) {
+        items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        items[i].style.top = (57 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    }
+    
+    document.querySelector('.menu-button').onclick = function(e) {
+        e.preventDefault();
+        document.querySelector('.circle').classList.toggle('open');
+    }
+});
