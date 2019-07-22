5aa6ee609dc7a8280aeebc1b483acf90c2041523
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-07-21 21:55:13 -0700

    Circle menu works on all pages

--- scripts/index.js
+++ scripts/index.js
@@ -12,16 +12,32 @@ function toggleMenu() {
     }
 }
 
+
 document.addEventListener("DOMContentLoaded", function(event) { 
+    // renderCircleMenu(0);
     const items = document.querySelectorAll('.circle a');
 
     for(let i = 0, l = items.length; i < l; i++) {
         items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
         items[i].style.top = (47 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        // items[i].onclick = function(e) {
+        //     const id = parseInt(items[i].id);
+        //     console.log(id);
+        //     renderCircleMenu(id);
+        // }
     }
-    
+
     document.querySelector('.menu-button').onclick = function(e) {
         e.preventDefault();
         document.querySelector('.circle').classList.toggle('open');
     }
 });
+
+// function renderCircleMenu(topIndex) {
+//     const items = document.querySelectorAll('.circle a');
+
+//     for(let i = 0, l = items.length; i < l; i++) {
+//         items[i].style.left = (50 - 35*Math.cos(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+//         items[i].style.top = (47 + 35*Math.sin(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+//     }
+// }
