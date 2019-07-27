f9423d16b193ce0e45875f5015716673d7350aee
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-07-27 16:05:15 -0700

    Updated site content and added features to mobile version

--- scripts/index.js
+++ scripts/index.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const items = document.querySelectorAll('.circle a');
 
     for(let i = 0, l = items.length; i < l; i++) {
-        items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-        items[i].style.top = (47 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        items[i].style.left = (50 - 36*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        items[i].style.top = (47 + 36*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
         // items[i].onclick = function(e) {
         //     const id = parseInt(items[i].id);
         //     console.log(id);
