4a08c69d25193a505a23539d2128aea39265e687
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-08-04 22:37:45 -0700

    Menu opens by default

--- scripts/index.js
+++ scripts/index.js
@@ -14,30 +14,19 @@ function toggleMenu() {
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    // renderCircleMenu(0);
-    const items = document.querySelectorAll('.circle a');
-
-    for(let i = 0, l = items.length; i < l; i++) {
-        items[i].style.left = (50 - 36*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-        items[i].style.top = (47 + 36*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-        // items[i].onclick = function(e) {
-        //     const id = parseInt(items[i].id);
-        //     console.log(id);
-        //     renderCircleMenu(id);
-        // }
-    }
-
+    renderCircleMenu(0);
+    document.querySelector('.circle').classList.toggle('open');
     document.querySelector('.menu-button').onclick = function(e) {
         e.preventDefault();
         document.querySelector('.circle').classList.toggle('open');
     }
 });
 
-// function renderCircleMenu(topIndex) {
-//     const items = document.querySelectorAll('.circle a');
+function renderCircleMenu(topIndex) {
+    const items = document.querySelectorAll('.circle a');
 
-//     for(let i = 0, l = items.length; i < l; i++) {
-//         items[i].style.left = (50 - 35*Math.cos(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-//         items[i].style.top = (47 + 35*Math.sin(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-//     }
-// }
+    for(let i = 0, l = items.length; i < l; i++) {
+        items[i].style.left = (50 - 35*Math.cos(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        items[i].style.top = (47 + 35*Math.sin(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    }
+}
