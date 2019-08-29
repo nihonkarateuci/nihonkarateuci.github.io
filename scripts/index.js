37db2f55fbc459419d6ba955f9d7fef12add5685
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-08-28 22:31:36 -0700

    Testing cookie setting

--- scripts/index.js
+++ scripts/index.js
@@ -14,12 +14,22 @@ function toggleMenu() {
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+    // Render circle menu
     renderCircleMenu(0);
+
+    // Open and close menu
     document.querySelector('.circle').classList.toggle('open');
     document.querySelector('.menu-button').onclick = function(e) {
         e.preventDefault();
         document.querySelector('.circle').classList.toggle('open');
     }
+
+    // Close announcement and set cookie
+    document.getElementById("close-announcement").addEventListener("click", function() {
+        const timeout = new Date(new Date() + 24*60*60*1000).toISOString();
+        document.cookie = "timeout=" + timeout;
+        document.getElementById("announcement").style.opacity = 0;
+    });
 });
 
 function renderCircleMenu(topIndex) {
@@ -28,5 +38,27 @@ function renderCircleMenu(topIndex) {
     for(let i = 0, l = items.length; i < l; i++) {
         items[i].style.left = (50 - 35*Math.cos(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
         items[i].style.top = (47 + 35*Math.sin(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    }
+}
+
+function getCookieData(key) {
+    const cookie = decodeURIComponent(document.cookie);
+    console.log(cookie);
+    return cookie.split(';').find(function(entry) {
+        let data = entry;
+        while (data.charAt(0) === ' ') {
+            data = data.substring(1);
+        }
+        return data.indexOf('timeout') === 0;
+    });
+}
+
+function hideAnnouncement() {
+    const timeout = getCookieData('timeout');
+    if (timeout !== undefined) {
+        timeoutEnd = timeout.substring('timeout'.length, timeout.length); // Extract expiration time
+        if (new Date(timeoutEnd) > new Date()) {
+            document.getElementById("announcement").style.display = 'none';
+        }
     }
 }
