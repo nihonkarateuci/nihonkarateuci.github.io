77bec39acbd0271a69d933597241469e018819ea
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-09-23 08:47:46 -0700

    Improved circle menu operation; Build script minifies CSS

--- build.py
+++ build.py
@@ -3,12 +3,25 @@ from datetime import datetime
 from pathlib import Path
 import subprocess
 import shutil
+import requests
 
-EXCLUDED_ITEMS = {Path('./build.py'), Path('./README.md'), Path('setup.sh'), Path('./defaults'), Path('./extras')}
+EXCLUDED_ITEMS = {Path('./build.py'), Path('./README.md'), Path('./setup.sh'), Path('./defaults'), Path('./extras')}
+
+def minify_dir(dir_name):
+    """Minifies all JS and CSS files in a diretory"""
+    for filename in dir_name.iterdir():
+        if filename.is_file() and filename.suffix in {'.js', '.css'}:
+            url = 'https://javascript-minifier.com/raw' if filename.suffix == 'js' else 'https://cssminifier.com/raw'
+            data = { 'input': filename.read_bytes() }
+            res = requests.post(url, data=data)
+            filename.write_text(res.text)
 
 def build_project(build_name):
     """Transfers required files into a new directory with the passed name and zips it."""
     shutil.copytree(Path('.'), Path(build_name), ignore=lambda dir, paths: [p for p in paths if Path(dir) in EXCLUDED_ITEMS or Path(p) in EXCLUDED_ITEMS])
+    # [TODO]: Get index.js to work with minification API
+    # minify_dir(Path(f'./{build_name}/scripts'))
+    minify_dir(Path(f'./{build_name}/styles'))
     subprocess.run(f'zip -r {build_name}.zip {build_name}', shell=True)
 
 def get_version_as_date():
@@ -18,7 +31,7 @@ def get_version_as_date():
 
 if __name__ == '__main__':
     # Generate a name for the build if one was not passed
-    version = sys.argv[1] if len(sys.argv) > 0 else get_version_as_date()
+    version = sys.argv[1] if len(sys.argv) >= 2 else get_version_as_date()
     build_name = f'nkcsite-build-{version}'
     try:
         print(f'Building NKC Website as {build_name}')
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Opens and closes mobile menu
 function toggleMenu() {
     let menu = document.getElementById("menu");
     let button = document.getElementById("toggle-button");
@@ -12,36 +13,36 @@ function toggleMenu() {
     }
 }
 
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-    // Render circle menu
-    renderCircleMenu(0);
-
-    // Open and close menu
+document.addEventListener("DOMContentLoaded", e => { 
+    // Set circle menu to open
     document.querySelector('.circle').classList.toggle('open');
+
+    // Add listener for closing menu
     document.querySelector('.menu-button').onclick = function(e) {
         e.preventDefault();
         document.querySelector('.circle').classList.toggle('open');
     }
 });
 
+// Renders the circle menu with the passed index at the top
 function renderCircleMenu(topIndex) {
     const items = document.querySelectorAll('.circle a');
 
-    for(let i = 0, l = items.length; i < l; i++) {
-        items[i].style.left = (50 - 35*Math.cos(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-        items[i].style.top = (47 + 35*Math.sin(Math.PI * topIndex/items.length - 0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    for(let i = 0, len = items.length; i < len; i++) {
+        items[i].style.left = (50 - 35*Math.cos(Math.PI * 2 * topIndex/len - 0.5 * Math.PI - 2*(1/len)*i*Math.PI)).toFixed(4) + "%";
+        items[i].style.top = (47 + 35*Math.sin(Math.PI * 2 * topIndex/len - 0.5 * Math.PI - 2*(1/len)*i*Math.PI)).toFixed(4) + "%";
     }
 }
 
+// Get the cookie with the passed key
 function getCookieData(key) {
     const cookie = decodeURIComponent(document.cookie);
-    return cookie.split(';').find(function(entry) {
+    return cookie.split(';').find(entry => {
         let data = entry;
         while (data.charAt(0) === ' ') {
             data = data.substring(1);
         }
-        return data.indexOf(key + '=') === 0;
+        return data.indexOf(key + '=') === 0; // Make sure the found instance is a key
     });
 }
 
@@ -50,16 +51,16 @@ function hideAnnouncement() {
     if (timeout !== undefined) {
         timeoutEnd = timeout.substring('timeout='.length, timeout.length); // Extract expiration time
         // Hide the announcement if the user has seen it within the last day
-	console.log(new Date(timeoutEnd) < new Date(), timeoutEnd);
         if (new Date(timeoutEnd) > new Date()) {
             document.getElementById("announcement").style.display = 'none';
         }
     }
 }
 
-// Close announcement and set cookie
+// Hide announcement and set announcement timeout cookie
 function closeAnnouncement() {
     const timeout = new Date();
+    // Timeout is 24 hours from time that announcement is closed
     timeout.setTime(timeout.getTime() + 24*60*60*1000);
     document.cookie = "timeout=" + timeout.toISOString();
     document.getElementById("announcement").style.opacity = 0;
