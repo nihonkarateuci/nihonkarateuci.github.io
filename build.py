0b8f7afd0db6c49c208a64f6334e33413675ae91
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2019-09-28 01:11:28 -0700

    New build script

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ def minify_dir(dir_name):
             res = requests.post(url, data=data)
             filename.write_text(res.text)
 
-def build_project(build_name):
+def create_build_dir(build_name):
     """Transfers required files into a new directory with the passed name and zips it."""
     shutil.copytree(Path('.'), Path(build_name), ignore=lambda dir, paths: [p for p in paths if Path(dir) in EXCLUDED_ITEMS or Path(p) in EXCLUDED_ITEMS])
     # [TODO]: Get index.js to work with minification API
@@ -29,14 +29,45 @@ def get_version_as_date():
     date = datetime.now()
     return '%04d%02d%02d' % (date.year, date.month, date.day)
 
-if __name__ == '__main__':
+def build():
+    """Builds the project for deployment"""
     # Generate a name for the build if one was not passed
-    version = sys.argv[1] if len(sys.argv) >= 2 else get_version_as_date()
+    version = sys.argv[2] if len(sys.argv) >= 3 else get_version_as_date()
     build_name = f'nkcsite-build-{version}'
     try:
         print(f'Building NKC Website as {build_name}')
-        build_project(build_name)
+        create_build_dir(build_name)
         print(f'Finished building {build_name}')
     except Exception as e:
         print(f'{type(e).__name__}: {e}')
         print('Failed to build NKC Website')
+
+def clean():
+    try:
+        print('Cleaning build_nae')
+    except Exception as e:
+        print(f'{type(e).__name__}: {e}')
+        print('Failed to build NKC Website')
+
+if __name__ == '__main__':
+    # Make sure an option was provided
+    if len(sys.argv) < 2:
+        print('Please provided')
+        sys.exit(1)
+    
+    # Possible commands
+    options = {
+        'build': build,
+        'clean': clean,
+    }
+
+    # F
+    operation = options.get(sys.argv[1])
+    if not operation:
+        print('Please prov')
+        sys.exit(1)
+    print('hi')
+    # else:
+        # operation()
+
+
