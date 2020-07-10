35f2f445bebf9b154d7ac30ce16d2c2388bf3cda
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2020-07-10 13:22:43 -0700

    Simplify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
+#!/bin/bash
+
 chmod 644 ./*.html
-chmod 755 ./scripts
-chmod 644 ./scripts/*.js
-chmod 755 ./styles
-chmod 644 ./styles/*.css
-chmod 755 ./images ./images/portraits ./images/banners ./images/home ./images/gallery
-chmod 644 ./images/*.jpg ./images/*.png ./images/portraits/*.jpg ./images/portraits/*.png ./images/banners/*.png ./images/home/*.jpg ./images/gallery/*.jpg
-chmod 755 ./resources
-chmod 644 ./resources/*.pdf
+chmod -R u+rwX,go+rX,go-w ./styles ./scripts ./images ./resources
