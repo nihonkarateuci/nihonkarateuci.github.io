aba23f5cd7941625dfb2ca8d930254adf69491da
caismw <83857355+caismw@users.noreply.github.com>
2022-09-18 23:04:09 -0700

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -35,7 +35,7 @@ NKCConfig.events = Object.freeze({
         path: "images/home/karatepracticetustin.jpg",
       },
       location: "Meeting at flagpoles",
-      date: new Date((year = 2022), (month = 08), (date = 28), (hours = 18)),
+      date: new Date((year = 2022), (month = 08), (date = 21), (hours = 18)),
       start_time: "6:00pm",
       end_time: "7:00pm",
       description:
